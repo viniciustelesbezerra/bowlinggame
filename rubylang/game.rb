edeaa0e038dc3ffa211bbda5863f1590c5a3eb44
vinicius teles <viniciusteles.bez@gmail.com>
2014-10-06 17:06:41 -0300

    sending dry code removing some ()

--- rubylang/game.rb
+++ rubylang/game.rb
@@ -3,7 +3,7 @@ class Game
     @rolls, @currentRoll = [], 0
   end
 
-  def roll(pins)
+  def roll pins
     @rolls.push pins
   end
 
@@ -11,10 +11,10 @@ class Game
     score, frameIndex = 0, 0
 
     10.times do
-      if (isStrike(frameIndex))
+      if isStrike(frameIndex)
         score += 10 + strikeBonus(frameIndex)
         frameIndex += 1
-      elsif (isSpare(frameIndex))
+      elsif isSpare(frameIndex)
         score += 10 + spareBonus(frameIndex)
         frameIndex += 2
       else
@@ -26,23 +26,23 @@ class Game
     score
   end
 
-  def isStrike(frameIndex)
+  def isStrike frameIndex
     @rolls[frameIndex] == 10
   end
 
-  def sumOfBallsInFrame(frameIndex)
+  def sumOfBallsInFrame frameIndex
     @rolls[frameIndex] + @rolls[frameIndex+1]
   end
 
-  def spareBonus(frameIndex)
+  def spareBonus frameIndex
     @rolls[frameIndex+2]
   end
 
-  def strikeBonus(frameIndex)
+  def strikeBonus frameIndex
     @rolls[frameIndex+1] + @rolls[frameIndex+2]
   end
 
-  def isSpare(frameIndex)
+  def isSpare frameIndex
     @rolls[frameIndex] + @rolls[frameIndex+1] == 10
   end
 end
