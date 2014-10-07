1821b9cbad32438ba5855c76d936924bc20e1bcf
vinicius teles <viniciusteles.bez@gmail.com>
2014-10-07 19:04:57 -0300

    sending some functions to complete the the game in jslang

--- jslang/spec/GameSpec.js
+++ jslang/spec/GameSpec.js
@@ -16,4 +16,22 @@ describe("Game", function() {
 
     expect(game.score()).toEqual(0);
   });
+
+  it("perfect game", function() {
+    for (var i = 0; i < 12; i++)
+      game.roll(10);
+
+    expect(game.score()).toEqual(300);
+  });
+
+  it("bonus spare", function() {
+    game.roll(5);
+    game.roll(5);
+    game.roll(2);
+
+    for (var i = 0; i < 17; i++)
+      game.roll(0);
+
+    expect(game.score()).toEqual(14);
+  });
 });
--- jslang/src/Game.js
+++ jslang/src/Game.js
@@ -11,15 +11,38 @@ var startGame = (function() {
     var score = 0, frameIndex = 0;
 
     for (var i = 0; i < 10; i++) {
-      score += this.sumOfBallsInFrame(frameIndex)
-      frameIndex += 2;
+      if(this.isStrike(frameIndex)) {
+        score += 10 + this.strikeBonus(frameIndex);
+        frameIndex += 1; }
+      else if(this.isSpare(frameIndex)) {
+        score += 10 + this.spareBonus(frameIndex);
+        frameIndex += 2; }
+      else {
+        score += this.sumOfBallsInFrame(frameIndex);
+        frameIndex += 2; }
     }
 
     return score;
   };
 
+  startGame.prototype.isStrike = function(frameIndex) {
+    return this.rolls[frameIndex] == 10;
+  }
+
   startGame.prototype.sumOfBallsInFrame = function(frameIndex) {
-    return this.rolls[frameIndex] + this.rolls[frameIndex+1]
+    return this.rolls[frameIndex] + this.rolls[frameIndex+1];
   };
+
+  startGame.prototype.spareBonus = function(frameIndex) {
+    return this.rolls[frameIndex+2];
+  }
+
+  startGame.prototype.strikeBonus = function(frameIndex) {
+    return this.rolls[frameIndex+1] + this.rolls[frameIndex+2];
+  }
+
+  startGame.prototype.isSpare = function(frameIndex) {
+    return this.rolls[frameIndex] + this.rolls[frameIndex+1] == 10;
+  }
 
   return startGame; })();
