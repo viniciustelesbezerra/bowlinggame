b799164bce48d2c3198b4df17fd72838d5105625
vinicius teles <viniciusteles.bez@gmail.com>
2014-10-07 19:13:12 -0300

    sending helper function to jslang rollforme

--- jslang/spec/GameSpec.js
+++ jslang/spec/GameSpec.js
@@ -3,35 +3,30 @@ describe("Game", function() {
 
   beforeEach(function() { game = new startGame; });
 
-  it("all ones", function() {
-    for (var i = 0; i < 20; i++)
-      game.roll(1);
+  var rollForMe = function(pins, times) {
+    times = times || 20;
+    for (var i = 0; i < times; i++) game.roll(pins);
+  };
 
+  it("all ones", function() {
+    rollForMe(1);
     expect(game.score()).toEqual(20);
   });
 
   it("all zeros", function() {
-    for (var i = 0; i < 20; i++)
-      game.roll(0); 
-
+    rollForMe(0);
     expect(game.score()).toEqual(0);
   });
 
   it("perfect game", function() {
-    for (var i = 0; i < 12; i++)
-      game.roll(10);
-
+    rollForMe(10 , 12);
     expect(game.score()).toEqual(300);
   });
 
   it("bonus spare", function() {
-    game.roll(5);
-    game.roll(5);
+    rollForMe(5, 2);
     game.roll(2);
-
-    for (var i = 0; i < 17; i++)
-      game.roll(0);
-
+    rollForMe(0 , 17);
     expect(game.score()).toEqual(14);
   });
 });
